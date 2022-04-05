5202dfc9672b60d6ba6d1f08c0e2a94b2c2934e7
Jaedotpy <101655762+Jaedotpy@users.noreply.github.com>
2022-04-05 10:57:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ if __name__ == "__main__":
     asignatura2 = Asignatura("Castellano", "Salon 201")
     grupo1 = Grupo()
 
-    print(asignatura1)
-    print(grupo1)
-    print(grupo1.grado)
+    print(asignatura1.getNombre())
+    print("Grupo de estudiantes: " + grupo1.getGrupo())
+    print("Grado", grupo1.getGrado())
 
     grupo2 = Grupo("Grupo 5", [], ["Alejandro", "Carlos"])
 
     grupo3 = Grupo()
     grupo4 = Grupo()
     grupo5 = Grupo()
-    grupo3.agregarAlumno("Kelly")
+    grupo3.agregarAlumno("Kelly", [])
     grupo4.agregarAlumno("Santiago", ["Jaime", "Pedro"])
-    grupo5.agregarAlumno("Javier")
+    grupo5.agregarAlumno("Javier", [])
 
-    print(grupo3.listadoAlumnos)
-    print(grupo4.listadoAlumnos)
-    print(grupo5.listadoAlumnos)
+
+    print(grupo3.getLista())
+    print(grupo4.getLista())
+    print(grupo5.getLista())
 
     grupo3.listadoAsignaturas(as1="Ciencias", as2="Quimica", as3="Ingles")
-    print(len(grupo3._asignaturas))
+    print(grupo3.getNumeroAsignaturas())
 
-    Grupo.asignarNombre("Grado 1")
-    print(Grupo.grado)
-    Grupo.asignarNombre()
-    print(Grupo.grado)
+    grupo1.asignarNombre("Grado 1")
+    print(grupo1.getGrupo())
+    grupo6 = Grupo()
+    grupo6.asignarNombre("Grado 6")
+    print(grupo6.getGrupo())
