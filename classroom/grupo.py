64cf0055814919809e9f2dbe314f3097b152b7fa
Jaedotpy <101655762+Jaedotpy@users.noreply.github.com>
2022-04-05 10:50:53 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,32 +6,37 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
-
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas= [], estudiantes=None, grado = 12):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
+        self._grado = grado
 
-    def listadoAsignaturas(self, kwargs):
+    def getGrupo(self):
+        return self._grupo
+    
+    def getGrado(self):
+        return self._grado
+
+    def getLista(self):
+        return self.listadoAlumnos
+
+    def getNumeroAsignaturas(self):
+        return len(self._asignaturas)
+
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
+        if lista is None:
             self.listadoAlumnos = [alumno]
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+        else:
+            lista.append(alumno)
+            if self.listadoAlumnos is None:
+                self.listadoAlumnos = lista
+            else:
+                self.listadoAlumnos  = self.listadoAlumnos + lista
+    
+    def asignarNombre(self, nombre):
+        self._grupo = nombre
